e72c054336099f94d7f1a834f7b0d49728355cbe
vikochnev <113198688+vikochnev@users.noreply.github.com>
2022-12-06 22:21:53 +0300

    updated test to check login url

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,10 +7,6 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    @staticmethod
-    def check_if_string_equals_split(string, *string_split):
-        return string.startswith(string_split[0][0]) and string.endswith(string_split[0][1])
-
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
