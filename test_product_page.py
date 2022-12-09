595da753833c6919d0261c7cf903ce94fd55e0cb
vikochnev <113198688+vikochnev@users.noreply.github.com>
2022-12-09 21:00:50 +0300

    added test to product page tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,8 @@
+import time
+
 import pytest
 
+from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
 
@@ -55,3 +58,12 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
