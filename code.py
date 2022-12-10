164eb4fb40fc6cf8b5865f05e4e64c2c1580955c
vikochnev <113198688+vikochnev@users.noreply.github.com>
2022-12-10 13:57:31 +0300

    deleted temporary file

--- code.py
+++
@@ -1,8 +0,0 @@
-string = "qwertyasdfd"
-string_split = ("qwerty", "asdfd")
-if_eq = string.startswith(string_split[0]) and string.endswith(string_split[1])
-# print(string_split[0])
-# print(string_split[1])
-# print(if_eq)
-
-
